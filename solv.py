dbd6f25bfa7a43e6ddf959c6d938c5c02cf8e775
markovicv <vukasinnauka@gmail.com>
2019-09-11 18:00:43 +0200

    board setup

---
+++ solv.py
@@ -0,0 +1,19 @@
+sudoku_board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+def print_board(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            print(board[i][j],end=" ")
+        print("")
+
+print_board(sudoku_board)
